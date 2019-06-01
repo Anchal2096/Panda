1d96ea55c91f7105e4345f03bd2a69c416edd4db
ANCHAL AITHANI <anchalaithani@gmail.com>
2019-06-01 09:41:22 +0530

    initial commit

---
+++ initial_panda_pgm.py
@@ -0,0 +1,13 @@
+# Load the Pandas libraries with alias 'pd'
+import pandas as pd
+import warnings
+
+# Control delimiters, rows, column names with read_csv
+df = pd.read_csv("business-operations-survey-2018-business-finance-csv.csv")
+# Preview the first 5 lines of the loaded df
+# print(df.head())
+print(df)
+print((df["value"]).max())  # to print the max value
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+print(df["industry"][df["level"] == 2])
